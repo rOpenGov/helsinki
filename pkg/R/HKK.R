299da8941a7ca868686a7a99510e62070914c7b3
antagomir <antagomir@opencomp>
2013-12-21 12:52:02 +0200

    HKK.R extending

--- pkg/R/HKK.R
+++ pkg/R/HKK.R
@@ -13,61 +13,34 @@
 #' @export
 #' @references
 #' See citation("helsinki") 
-#' @author Joona Lehtomaki \email{louhos@@googlegroups.com}
-#' @examples # sp <- GetHKK("Aanestysaluejako", data.dir=".")
+#' @author Joona Lehtomaki and Leo Lahti \email{louhos@@googlegroups.com}
+#' @examples # sp <- GetHKK("Aanestysaluejako")
 
-GetHKK <- function(which.data, data.dir) {
+GetHKK <- function( which.data ) {
   
-  # FIXME: Harmonize functionality with `GetHSY`
-  #@param `which.data` a string with one of the values `"Aluejakorajat"`, `"Aanesysaluejako"` or `"Seutukartta"`  
-  #@return **Undecided**: either (1) a Shape object or (2) a list of Shape objects (from `SpatialPolygonsDataFrame` class)
-  #NOTE: in sorvi 0.1.44 data set Aluejakorajat is available through `GetHRIaluejakokartat`. This functionality could be merged to `GetHKK` in the future.
-## Work stages
-
-  #1. Download the requested data set from HKK-webpages
-  #2. Unzip the data set into a predefined location [SORVI_PKG_LOCATION]/extdata (should the location be included in the function call?)
-  # At this point the pre-processing of the datasets diverge  
-
-  # `Aanestysaluejako`
-  # Data set includes the elections districts for the cities of the capital region (Helsinki, Espoo, Kauniainen and Vantaa) in MapInfo-format (OGR will handle these just fine). CRS is Finland KKJ2 (EPSG: 2392). All feature datasets have different schemas with varying amount of information. Additionally there is an Excel file (Aanestyaluekoodit.xls) with information for all cities, such as voting district names and codes.
-
-  #1. Extract the information for each city from Excel-sheets into data frames (`read.xls`)
-  # 2. Pre-process the fields in the voting information (break down fields, handle encoding etc.)
-  # 3. Read in the spatial data for each city (`readOGR`)
-  # 4. Merge data from the voting information tables and the spatial data
-  # 5. Either  
-  #* return a list of `SpatialPolygonDataframes`  
-  #* Merge all cities into 1 sp-object. This could be done e.g. using `spRbind`. Note that biding the sp-objects would call for defining unique FIDs for all the polygons using `spChFIDs` on the voting district code
-
-
-
-  #.InstallMarginal("rgdal")
-  
-  # TODO: shold all the urls/paths be defined independently from the functions?
   data.url <- "http://kartta.hel.fi/avoindata/aineistot/"
-  
-  # Create data.dir if does not exist
-  # FIXME use temporary data dir
-  if (!file.exists(data.dir))
-    dir.create(data.dir)
-  
+  data.dir <- tempdir()  
+
   if (which.data == "Aanestysaluejako") {
-    
+
     # Remote zip to download
     remote.zip <- "pk_seudun_aanestysalueet.zip"
+    unzip.files(data.dir, remote.zip, local.zip, data.url, which.data)
 
-    # Local zip location 
-    local.zip <- file.path(data.dir, remote.zip)
+    # dir(data.dir)
+    # Paakaupunkiseudun aanestysalueiden tunnukset ja nimet Tilastokeskuksen 
+    # StatFin-vaalitietopalvelussa 2012. Not used in this function for now.
+    # Since these seem to be in the final shapefile anyway.
+    #"Aanestysaluekoodit.xls" 
 
-    # Create the web address from where to fetch the zip
-    data.url <- paste(data.url, remote.zip, sep = "")
-    message(paste("Dowloading HKK data from ", data.url, "in file", local.zip))
-    download.file(data.url, destfile = local.zip)
+    #"Avoin lisenssi aanestysalueet.pdf" 
+    #"PKS_aanestysalueet_kkj2.DAT" -> Associated with the TAB file?      
+    #"PKS_aanestysalueet_kkj2.ID"  -> Associated with the TAB file?      
+    #"PKS_aanestysalueet_kkj2.MAP" -> Associated with the TAB file?      
+    #"PKS_aanestysalueet_kkj2.TAB" -> Used in this function     
+    #"PKS_aanestysalueet_kkj2.txt" -> Skipped for now, presumably the 
+    #                                 same information as in the mapinfo files
 
-    # Unzip the downloaded zip file
-    data.dir <- file.path(data.dir, "aanestysalueet")
-    unzip(local.zip, exdir=data.dir)
-    
     mapinfo.file <- file.path(data.dir, "PKS_aanestysalueet_kkj2.TAB")
     
     sp.cities <- rgdal::readOGR(mapinfo.file, 
@@ -78,17 +51,137 @@ GetHKK <- function(which.data, data.dir) {
                                           from="ISO-8859-1", to="UTF-8"))
     sp.cities@data$Nimi <- factor(iconv(sp.cities@data$Nimi, 
                                         from="ISO-8859-1", to="UTF-8"))
-    # Remove TEMP file
-    unlink("TEMP", recursive=T)
+    # Remove temporary directory
+    unlink(data.dir, recursive=T)
     
     return(sp.cities)
     
-  } else if (which.data == "Aluejakokartat") {
+  } else if (which.data == "Aluejakorajat") {
+
+    # Aluejakorajat:
+    # http://kartta.hel.fi/avoindata/aineistot/PKS_Kartta_Rajat_KML2011.zip
+
+    #> dir(data.dir)
+    # "Kartta_avoindata_kayttoehdot_v02_3_2011.pdf"
+    # "PKS_aluejako_alueet_ja_pisteet.kml"         
+    # "PKS_kunta.kml"                              
+    # "PKS_pienalue.kml"                           
+    # "PKS_pienalue_piste.kml"                     
+    # "PKS_suuralue.kml"                           
+    # "PKS_suuralue_piste.kml"                     
+    # "PKS_tilastoalue.kml"                        
+    # "PKS_tilastoalue_piste.kml"         
+
+    # Remote zip to download
+    remote.zip <- "PKS_Kartta_Rajat_KML2011.zip"
+    unzip.files(data.dir, remote.zip, local.zip, data.url, which.data)
+
+    #mapinfo.file <- file.path(data.dir, "PKS_aanestysalueet_kkj2.TAB")
+    #sp.cities <- rgdal::readOGR(mapinfo.file, 
+    #                             layer = rgdal::ogrListLayers(mapinfo.file))
+    # Fix encoding
+    #sp.cities@data$TKNIMI <- factor(iconv(sp.cities@data$TKNIMI, 
+    #                                      from="ISO-8859-1", to="UTF-8"))
+
+    # Remove temporary directory
+    #unlink(data.dir, recursive=T)
+    
+    #return(sp.cities)
+
     stop(paste(which.data, "not implemented. Try GetHRIaluejakokartat instead."))
+
+    # NOTE: in sorvi 0.1.44 data set Aluejakorajat is available through 
+    # `GetHRIaluejakokartat`. This functionality could be merged to 
+    # `GetHKK` in the future.
+
   } else if (which.data == "Seutukartta") {
+
+    # Seutukartta: 
+    # http://kartta.hel.fi/avoindata/aineistot/sk11_avoin.zip
+    
+    # Remote zip to download
+    remote.zip <- "sk11_avoin.zip"
+    unzip.files(data.dir, remote.zip, local.zip, data.url, which.data)
+
+    message(paste(which.data, "contains a number of MAPINFO files. To be implemented."))
+
     stop(paste(which.data, "not implemented"))
+
+  } else if (which.data == "Seudullinen osoiteluettelo") {
+
+    # Seudullinen osoiteluettelo
+    # http://kartta.hel.fi/avoindata/aineistot/Seudullinen_osoiteluettelo.zip
+    remote.zip <- "Seudullinen_osoiteluettelo.zip"
+    unzip.files(data.dir, remote.zip, local.zip, data.url, which.data)
+
+    # "Seudullinen_osoiteluettelo.csv"             
+    # "Seudullisen_Avoimen_osoitetiedon_kuvaus.pdf"
+
+    stop(paste(which.data, "not implemented"))
+
+  } else if (which.data == "Helsingin osoiteluettelo") {
+
+    # Helsingin osoiteluettelo:
+    # http://kartta.hel.fi/avoindata/aineistot/Helsingin_osoiteluettelo.zip
+
+    remote.zip <- "Helsingin_osoiteluettelo.zip"
+    unzip.files(data.dir, remote.zip, local.zip, data.url, which.data)
+
+    # "Helsingin_osoiteluettelo.csv"    
+
+    stop(paste(which.data, "not implemented"))
+
+  } else if (which.data == "Helsingin piirijako") {
+
+    # Helsingin piirijako:
+    # http://kartta.hel.fi/avoindata/aineistot/Helsingin_piirijako_2013.zip
+
+    remote.zip <- "Helsingin_piirijako_2013.zip"
+    unzip.files(data.dir, remote.zip, local.zip, data.url, which.data)
+
+    message(paste(which.data, "contains a number of MAPINFO files. To be implemented."))
+    stop(paste(which.data, "not implemented"))
+
+  } else if (which.data == "Helsingin kaupungin rakennusrekisterin ote") {
+
+    # Helsingin kaupungin rakennusrekisterin ote:
+    # http://kartta.hel.fi/avoindata/aineistot/rakennukset_Helsinki_06_2012.zip
+
+    remote.zip <- "rakennukset_Helsinki_06_2012.zip"
+    unzip.files(data.dir, remote.zip, local.zip, data.url, which.data)
+
+    # "Metatieto_rakennukset.xls"
+    # "rakennukset_helsinki_20m2_hkikoord" 
+    # "rakennukset_Helsinki_etrsgk25"     
+    # "rakennukset_Helsinki_hkikoord"      
+    # "rakennukset_Helsinki_wgs84"        
+
+    message(paste(which.data, "contains a number of MAPINFO files. To be implemented."))
+    stop(paste(which.data, "not implemented"))
+
   } else {
+
     stop(paste(which.data, "is not a valid data set descriptor"))
+
   }
+
 }
 
+
+unzip.files <- function (data.dir, remote.zip, local.zip, data.url, which.data) {
+
+    # Local zip location 
+    local.zip <- file.path(data.dir, remote.zip)
+
+    # Create the web address from where to fetch the zip
+    data.url <- paste(data.url, remote.zip, sep = "")
+    message(paste("Dowloading HKK", which.data, " data from ", data.url, "in file", local.zip, ". Kindly cite the original data sources. For license and citation information, see http://kartta.hel.fi/avoindata/"))
+    download.file(data.url, destfile = local.zip)
+
+    # Unzip the downloaded zip file
+    #data.dir <- file.path(data.dir, "aanestysalueet")
+    unzip(local.zip, exdir = data.dir)
+
+}
+ 
+   

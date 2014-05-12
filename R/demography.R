328287b5a472c7146b3b81504c5791a15e6cc630
ouzor <ouzor@iki.fi>
2014-05-12 15:25:09 +0300

    Added demographic population projection data.

---
+++ R/demography.R
@@ -0,0 +1,47 @@
+# This file is a part of the helsinki package (http://github.com/rOpenGov/helsinki)
+# in association with the rOpenGov project (ropengov.github.io)
+
+# Copyright (C) 2010-2014 Juuso Parkkinen / Louhos <louhos.github.com>. 
+# All rights reserved.
+
+# This program is open source software; you can redistribute it and/or modify 
+# it under the terms of the FreeBSD License (keep this notice): 
+# http://en.wikipedia.org/wiki/BSD_licenses
+
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+
+#' Retrieve population projection data
+#'
+#' Retrieve population projection data (vaestoennuste) by age and gender
+#' from Helsinki Region Infoshare
+#' http://www.hri.fi/fi/data/helsingin-ja-helsingin-seudun-vaestoennuste-sukupuolen-ja-ian-mukaan-2012-2050/
+#' Helsinki, Helsinki region, 2012-2050
+#'
+#' @param verbose logical. Should R report extra information on progress? 
+#' @return results A list with indicator data as a data frame,
+#' and additional metadata provided in the original data.
+#' 
+#' @export
+#' 
+#' @references See citation("helsinki") 
+#' @author Juuso Parkkinen \email{louhos@@googlegroups.com}
+#' @examples res.list <- get_population_projection()
+
+get_population_projection <- function (verbose=TRUE) {
+  
+  if (verbose)
+    message("Reading population projections for Helsinki and Helsinki region, years 2012-2050.")
+  
+  # Read csv from http://www.hri.fi/fi/data/helsingin-ja-helsingin-seudun-vaestoennuste-sukupuolen-ja-ian-mukaan-2012-2050/
+  csv.url <- "http://www.hel2.fi/tietokeskus/data/helsinki/Vaestoennusteet/Helsingin_seudun_vaestoennuste_ika_sp_2012_2050.csv"
+  pop.proj <- read.csv(csv.url, sep=";", fileEncoding="ISO-8859-1", skip=3, stringsAsFactors=FALSE)
+  
+  # Read metadata
+  meta.raw <- read.csv(csv.url, sep=";", header=FALSE, fileEncoding="ISO-8859-1", nrow=3, stringsAsFactors=FALSE)
+  return(list(data=pop.proj, metadata=meta.raw$V1))
+}
+
+
--- R/economy.R
+++ R/economy.R
@@ -34,7 +34,8 @@ get_economic_indicators <- function (verbose=TRUE) {
 
   ## Economic indicators; Hel, Esp, Van, Kau, Koko maa; years 1998-2010
   if (verbose)
-    message("Reading economic indicators\nHelsinki, Espoo, Vantaa, Kauniainen, Finland\nYears 1998-2010.")
+    message("Reading economic indicators for Helsinki region, years 1998-2010.")
+  
   # Read csv from http://www.hri.fi/fi/data/paakaupunkiseudun-kuntien-taloudellisia-tunnuslukuja/
   csv.url <- "http://www.hel2.fi/tietokeskus/data/talous/PKS_kuntien_taloustunnuslukuja_1.csv"
   ec.ind <- read.csv(csv.url, sep=";", fileEncoding="ISO-8859-1", skip=2, nrow=160, stringsAsFactors=FALSE)
@@ -44,10 +45,13 @@ get_economic_indicators <- function (verbose=TRUE) {
   ec.ind[3:15] <- lapply(ec.ind[3:15], function(col) as.numeric(gsub(",", ".", col)))
   # Fix year names
   names(ec.ind)[3:15] <- gsub("X", "", names(ec.ind)[3:15])
-  # Read metadata
-  met1.raw <- read.csv(csv.url, sep=";", header=FALSE, fileEncoding="ISO-8859-1", nrow=2, stringsAsFactors=FALSE)
-  met2.raw <- read.csv(csv.url, sep=";", header=FALSE, fileEncoding="ISO-8859-1", skip=165, stringsAsFactors=FALSE)
-  return(list(indicators=ec.ind, metadata=met1.raw$V1, notes=met2.raw$V1))
+  
+  # Read metadata and notes
+  meta.raw <- read.csv(csv.url, sep=";", header=FALSE, fileEncoding="ISO-8859-1", nrow=2, stringsAsFactors=FALSE)
+  notes.raw <- read.csv(csv.url, sep=";", header=FALSE, fileEncoding="ISO-8859-1", skip=165, stringsAsFactors=FALSE)
+  
+  # Return list with data, metadata, and notes
+  return(list(data=ec.ind, metadata=meta.raw$V1, notes=notes.raw$V1))
 }
 
 

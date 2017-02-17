b458337dd1541d33d17dc38840f2416f2fcd7c14
antagomir <leo.lahti@iki.fi>
2017-02-17 11:44:13 +0200

    Adding vignettebuilder and polishing DESCRIPTION file to meet latest recommendations

--- inst/extras/build.cran.sh
+++ inst/extras/build.cran.sh
@@ -1,4 +1,4 @@
 /usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../
-/usr/bin/R CMD check --as-cran helsinki_0.9.21.tar.gz
-/usr/bin/R CMD INSTALL helsinki_0.9.21.tar.gz
+/usr/bin/R CMD check --as-cran helsinki_0.9.28.tar.gz
+/usr/bin/R CMD INSTALL helsinki_0.9.28.tar.gz

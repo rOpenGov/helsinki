60806ef547f46f4539490f703eaec4942877dacb
antagomir <leo.lahti@iki.fi>
2017-02-17 11:58:01 +0200

    Switched off get_hsy examples

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,6 +1,6 @@
 # This file is a part of rOpenGov (http://ropengov.github.io)
 
-# Copyright (C) 2010-2014 Leo Lahti, Juuso Parkkinen and Joona Lehtomaki <ropengov.github.com>. 
+# Copyright (C) 2010-2017 Leo Lahti, Juuso Parkkinen and Joona Lehtomaki <ropengov.github.com>. 
 # All rights reserved.
 
 # This program is open source software; you can redistribute it and/or modify 
@@ -19,6 +19,6 @@
   # Sys.setlocale(locale = "UTF-8")
   # Sys.setlocale(locale = "WINDOWS-1252")
 
-  packageStartupMessage("\nhelsinki R package: tools for open data from Helsinki, Finland capital region.\nThis R package is part of rOpenGov <ropengov.github.io>.\nCopyright (C) 2010-2014 Juuso Parkkinen, Leo Lahti and Joona Lehtomaki.\nThis is free software. You are free to use, modify and redistribute it under the FreeBSD license.")
+  packageStartupMessage("\nhelsinki R package: tools for open data from Helsinki, Finland capital region.\nThis R package is part of rOpenGov <ropengov.github.io>.\nCopyright (C) 2010-2017 Juuso Parkkinen, Leo Lahti and Joona Lehtomaki.\nThis is free software. You are free to use, modify and redistribute it under the FreeBSD license.")
 
 }

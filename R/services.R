3e78c0b199b0a566ca6f992dad5c51866a3df959
Pyry Kantanen <pyry.kantanen@utu.fi>
2021-04-23 20:24:27 +0300

    Remove rjson and RCurl dependencies, return data frames instead of lists

--- R/services.R
+++ R/services.R
@@ -13,81 +13,36 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
 
-#' Access Helsinki region Service Map API
+#' @title Access Helsinki region Service Map API
 #'
 #' Access the new Helsinki region Service Map (Paakaupunkiseudun Palvelukartta)
-#' http://dev.hel.fi/servicemap/ data through the API: http://api.hel.fi/servicemap/v1/. 
+#' http://dev.hel.fi/servicemap/ data through the API: http://api.hel.fi/servicemap/v2/. 
 #' For more API documentation and license information see the API link.
 #' 
 #' @param query The API query as a string, for example "search", "service", or "unit".
-#' For full list of available options and details, see http://api.hel.fi/servicemap/v1/. 
+#' For full list of available options and details, see http://api.hel.fi/servicemap/v2/. 
 #' @param ... Additional parameters to the API (optional).
-#' For details, see http://api.hel.fi/servicemap/v1/. 
+#' For details, see http://api.hel.fi/servicemap/v2/. 
 #'
-#' @return List of results
-#' @export
-#' @importFrom RCurl getCurlHandle
-#' @importFrom RCurl getForm
-#' @importFrom rjson fromJSON
+#' @return Data frame
 #' 
-#' @author Juuso Parkkinen \email{louhos@@googlegroups.com}
-#' @examples search.puisto <- get_servicemap(query="search", q="puisto")
+#' @importFrom httr parse_url build_url
+#' @importFrom jsonlite fromJSON
+#' 
+#' @author Juuso Parkkinen \email{louhos@@googlegroups.com}, Pyry Kantanen
+#' @examples 
+#' search.puisto <- get_servicemap(query="search", q="puisto")
+#' 
+#' @export
 
 get_servicemap <- function(query, ...) {
   
-  # api.url <- "http://www.hel.fi/palvelukarttaws/rest/v2/"
-  # Define query url
-  # New API (13.5.2014)
-  api.url <- "http://api.hel.fi/servicemap/v1/"
+  api_url <- "http://api.hel.fi/servicemap/v2/"
+  query_url <- paste0(api.url, query, "/")
   
-  # Check whether API url available
-  if (!RCurl::url.exists(api.url)) {
-    message(paste("Sorry! API", api.url, "not available!\nReturned NULL."))
-    return(NULL)
-  }
-  
-  query.url <- paste0(api.url, query, "/")
-  
-  # Get Curl handle
-  curl <- RCurl::getCurlHandle(cookiefile = "")
-  
-  # Get data as json using getForm
-  # Note! Warnings suppressed because getForm outputs warning when no parameters (...) given
-  suppressWarnings(
-    res.json <- RCurl::getForm(uri=query.url, ..., curl=curl)
-  )
-  # Transform results into list from JSON
-  res.list <- rjson::fromJSON(res.json)
-  return(res.list)
-}
-
-
-
-#' Access Helsinki Linked Events API
-#'
-#' Access the new Helsinki Linked Events API: http://api.hel.fi/linkedevents/v0.1/.
-#' The API contains data from the Helsinki City Tourist & Convention Bureau, 
-#' the City of Helsinki Cultural Office and the Helmet metropolitan area public libraries.
-#' For more API documentation and license information see the API link.
-#' 
-#' @param query The API query as a string, one of "category", "event", "language", or "place".
-#' For details, see http://api.hel.fi/linkedevents/v0.1/. 
-#' @param ... Additional parameters to the API (optional).
-#' For details, see http://api.hel.fi/linkedevents/v0.1/. 
-#'
-#' @return List of results
-#' @export
-#' @importFrom RCurl getCurlHandle
-#' @importFrom RCurl getForm
-#' @importFrom rjson fromJSON
-#' 
-#' @author Juuso Parkkinen \email{louhos@@googlegroups.com}
-#' @examples events <- get_linkedevents(query="event")
-
-get_linkedevents <- function(query, ...) {
-  
-  # Define query url
-  api.url <- "http://api.hel.fi/linkedevents/v0.1/"
+  url <- httr::parse_url(query_url)
+  url$query <- list(...)
+  url <- httr::build_url(url)
   
   # Check whether API url available
   conn<-url(api.url)
@@ -97,20 +52,58 @@ get_linkedevents <- function(query, ...) {
     warning(paste("Sorry! API", api.url, "not available! Returning NULL"))
     return(NULL)
   }
-  
-  query.url <- paste0(api.url, query, "/")
-  
-  # Get Curl handle
-  curl <- RCurl::getCurlHandle(cookiefile = "")
-  
-  # Get data as json using getForm
-  # Note! Warnings suppressed because getForm outputs warning when no parameters (...) given
-  suppressWarnings(
-    res.json <- RCurl::getForm(uri=query.url, ..., curl=curl)
-  )
-  # Transform results into list from JSON
-  res.list <- rjson::fromJSON(res.json)
-  return(res.list)
+
+  res_list <- jsonlite::fromJSON(url)
+  res_list <- res_list$data 
+  return(res_list)
 }
 
 
+
+#' @title Access Helsinki Linked Events API
+#'
+#' Access the new Helsinki Linked Events API: http://api.hel.fi/linkedevents/v0.1/.
+#' The API contains data from the Helsinki City Tourist & Convention Bureau, 
+#' the City of Helsinki Cultural Office and the Helmet metropolitan area public libraries.
+#' For more API documentation and license information see the API link.
+#' 
+#' @param query The API query as a string, one of "category", "event", "language", or "place".
+#' For details, see http://api.hel.fi/linkedevents/v1/. 
+#' @param ... Additional parameters to the API (optional).
+#' For details, see http://api.hel.fi/linkedevents/v1/. 
+#'
+#' @return Data frame
+#' 
+#' @importFrom httr parse_url build_url
+#' @importFrom jsonlite fromJSON
+#' 
+#' @author Juuso Parkkinen \email{louhos@@googlegroups.com}, Pyry Kantanen
+#' 
+#' @examples 
+#' events <- get_linkedevents(query="search", q="teatteri", start="2020-01-01")
+#' 
+#' @export
+
+get_linkedevents <- function(query, ...) {
+  
+  # Build query url
+  api_url <- "http://api.hel.fi/linkedevents/v1/"
+  query_url <- paste0(api.url, query, "/")
+  
+  url <- httr::parse_url(query_url)
+  url$query <- list(...)
+  url <- httr::build_url(url)
+  
+  # Check whether API url available
+  conn<-url(api.url)
+  doesnotexist<-inherits(try(suppressWarnings(readLines(conn)),silent=TRUE),"try-error")
+  close(conn)
+  if (doesnotexist) {
+    warning(paste("Sorry! API", api.url, "not available! Returning NULL"))
+    return(NULL)
+  }
+
+  res_list <- jsonlite::fromJSON(url)
+  res_list <- res_list$data 
+  return(res_list)
+}
